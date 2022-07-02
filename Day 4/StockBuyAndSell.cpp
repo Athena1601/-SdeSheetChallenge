2fa36de67195ff333ecd73e8960caa0126493fde
Athena1601 <95461317+Athena1601@users.noreply.github.com>
2022-07-02 09:33:36 +0530

    Update StockBuyAndSell.cpp

--- Day 4/StockBuyAndSell.cpp
+++ Day 4/StockBuyAndSell.cpp
@@ -1,1 +1,15 @@
 
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    // Write your code here.
+    
+    int max_profit = 0;
+    int min_price = INT_MAX;
+    
+    for(int i=0;i<prices.size();i++)
+    {
+        min_price = min(min_price,prices[i]);
+        max_profit = max(max_profit,prices[i]-min_price);
+    }
+    return max_profit;
+}
