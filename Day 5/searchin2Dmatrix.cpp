bcd3e56c40170a356c92022a692cf1236121e860
Athena1601 <95461317+Athena1601@users.noreply.github.com>
2022-07-02 09:36:09 +0530

    Update searchin2Dmatrix.cpp

--- Day 5/searchin2Dmatrix.cpp
+++ Day 5/searchin2Dmatrix.cpp
@@ -1,1 +1,30 @@
 
+#include <bits/stdc++.h> 
+#include <vector>
+
+bool findTargetInMatrix(vector < vector < int >> & mat, int m, int n, int target) {
+    // Write your code here.
+    if(mat.size()==0)
+        return false;
+    
+    int low = 0;
+    int high = (m*n)-1;
+    
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+        if(mat[mid/n][mid%n]==target)
+        {
+            return true;
+        }
+        else if(mat[mid/n][mid%n]<target)
+        {
+            low=mid+1;
+        }
+        else if(mat[mid/n][mid%n]>target)
+        {
+            high = mid-1;
+        }
+    }
+    return false;
+}
