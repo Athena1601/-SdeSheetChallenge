d5d55aea67f1d332cacf8f6f2faafc675eb14073
Athena1601 <95461317+Athena1601@users.noreply.github.com>
2022-07-03 20:42:43 +0530

    Update Pow(X,n).cpp

--- Day 6/Pow(X,n).cpp
+++ Day 6/Pow(X,n).cpp
@@ -1,1 +1,17 @@
 
+
+#include <bits/stdc++.h> 
+int modularExponentiation(int x, int n, int m) {
+	// Write your code here.
+    long long int result = 1;
+    
+    while(n>0)
+    {
+        if(n&1)
+            result = (result*(x)%m)%m;
+        
+        x = (1LL*(x)%m*(x)%m)%m;
+        n=n>>1;
+    }
+    return result;
+}
